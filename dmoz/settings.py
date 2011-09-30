f1bac5cd6c07364e08a6cdc61c13134c33b37853
Junjay Tan <jtan@peregrinegroup.com>
2011-09-30 16:05:48 -0400

    initial commit

---
+++ dmoz/settings.py
@@ -0,0 +1,16 @@
+# Scrapy settings for dmoz project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'dmoz'
+BOT_VERSION = '1.0'
+
+SPIDER_MODULES = ['dmoz.spiders']
+NEWSPIDER_MODULE = 'dmoz.spiders'
+DEFAULT_ITEM_CLASS = 'dmoz.items.DmozItem'
+USER_AGENT = '%s/%s' % (BOT_NAME, BOT_VERSION)
+
