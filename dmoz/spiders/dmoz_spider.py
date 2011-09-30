c9c4b9af2dfc30d191fa601b980b74f770cf9d4d
Junjay Tan <jtan@peregrinegroup.com>
2011-09-30 17:51:12 -0400

    fixed so it selected the right ul class

--- dmoz/spiders/dmoz_spider.py
+++ dmoz/spiders/dmoz_spider.py
@@ -13,12 +13,13 @@ class DmozSpider(BaseSpider):
 
 	def parse(self, response):
 		hxs = HtmlXPathSelector(response) #initiate the selectors
-		sites = hxs.select('//ul/li')
+		sites = hxs.select('//ul[@class="directory-url"]/li')  #change this to select ul of class "directory-url"
 		items = []		
 		for site in sites:
 			item = DmozItem()
 			item['title'] = site.select('a/text()').extract()
 			item['link'] = site.select('a/@href').extract()
-			item['desc'] = site.select('text()').extract()
+			item['desc'] = site.select('text()').extract() #something is still wrong with description since its collecting the \t and \n elements
+#i.e., ignore spaces, tabs, and new line breaks
 			items.append(item)
 		return items
